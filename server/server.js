4b597d15a4cba8ea1db2f90389b7c1e7df8c3ebd
Yaroslav <yarais@MacBook-Pro-Apple.local>
2017-08-09 17:42:16 -0700

    Message events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,26 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=> {
   console.log('New user connected');
 
+  socket.emit('newEmail', {
+    from: 'Me',
+    text: 'Test text'
+  });
+
+  socket.emit('newMessage', {
+    from: 'User2',
+    text: 'Test text!',
+    createdAt: new Date().getTime()
+  });
+
+  socket.on('createEmail', (email) => {
+    console.log(email);
+  });
+
+  socket.on('createMessage', (message) => {
+    message.createdAt = new Date().getTime();
+    console.log(message);
+  });
+
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
